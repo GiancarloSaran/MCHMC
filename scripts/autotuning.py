b23f5978ce4e81e78ee640822a52dc87f7c4127a
gaiavolpi <gaia.volpi@studenti.unipd.it>
2025-05-08 14:03:42 +0200

    changes in epsilon tuning

--- scripts/autotuning.py
+++ scripts/autotuning.py
@@ -4,29 +4,24 @@ import numpy as np
 from utils import checkpoint, warning
 import MCHMC
 
-def tune_eps(d, L, fn, N, iterations=10, debug=False):
+def tune_eps(d, N, L, fn, iterations=10, debug=True):
     checkpoint("Tuning epsilon..", debug=debug)
-    checkpoint(f"\nParameters\n\td={d}\n\tL={L}\n\tNn={N}")
-
     epsilon = 0.5 # initial value
-    checkpoint(f"Initial value: epsilon = {epsilon}")
 
     eps_values = torch.zeros(iterations)
-    #Change to an "until convergence" criterion, could track ((eps_i+1 - eps_i)/eps_i)^2 and get it lower
-    #than a threshold
+    #Change to an "until convergence" criterion, could track ((eps_i+1 - eps_i)/eps_i)^2 and 
+    #get it lower than a threshold
     checkpoint(f"\nRunning multiple iterations of MCHMC_bounces with {N} steps, updating epsilon")
     for i in range(iterations):
-        checkpoint(f"\n---------------------------\nIteration {i}")
-        X, E, *_ = MCHMC.MCHMC_bounces(d, N, L, epsilon, fn, debug=debug)
-        checkpoint(f"X.shape: {X.shape}, E.shape: {E.shape}")
+        X, E, *_ = MCHMC_bounces(d, N, L, epsilon, fn, debug=debug)
         varE = E.var()
-        checkpoint(f"varE: {varE}")
+        #checkpoint(f"varE: {varE}")
         epsilon *= (0.0005 * d / varE)**(1/4)
-        checkpoint(f"New epsilon: {epsilon}")
+        checkpoint(f"Iteration {i} epsilon: {epsilon}")
         eps_values[i]=epsilon
     checkpoint(f"\tOptimized epsilon: {epsilon}")
 
-    return eps_values, X, E
+    return eps_values
 
 def s_eff(X):
   return np.sqrt((X**2).var(axis=0).mean())
